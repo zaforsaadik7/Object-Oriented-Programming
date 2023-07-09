4633166b01de4c6d0ec2efc2090203a50b025fa1
Emam Zafor <121710926+zaforsaadik7@users.noreply.github.com>
2023-07-09 09:36:19 +0600

    Create Write a C++ program that print.cpp

---
+++ Write a C++ program that print.cpp
@@ -0,0 +1,10 @@
+/*  Write a C++ program that prints:
+ /'Bangladesh University of Business and Technology”\
+Hello class of CSE121!! Welcome to BUBT!   */
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"/'Bangladesh University of Business and Technology”\ \n Hello class of CSE121!! Welcome to BUBT!";
+return 0;
+}
