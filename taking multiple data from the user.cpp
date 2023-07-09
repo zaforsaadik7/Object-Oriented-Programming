f5ff8122d6ed5c20a0c70bdaaae2dec1f39ab108
Emam Zafor <121710926+zaforsaadik7@users.noreply.github.com>
2023-07-09 09:01:41 +0600

    Create taking multiple data from the user.cpp

---
+++ taking multiple data from the user.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    float j;
+    char s[80];
+    cout<<"Enter a integer, float and a string:";
+    cin>>i>>j>>s;
+    cout<<"Here is your data:"<<i<<' '<<' '<<j<<' '<<s<<endl;
+    return 0;
+}
