8b644c245e2e360df4f62e2409a65de373b0f59a
Emam Zafor <121710926+zaforsaadik7@users.noreply.github.com>
2023-07-10 22:05:07 +0600

    Create Print N to 1 using while loop.cpp

---
+++ Print N to 1 using while loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    while(n!= 0)
+    {
+        cout<<n;
+        n--;
+    }
+    return 0;
+}
