0bf7ca27a9da269f485a1fd24b5f73563ea528ac
Emam Zafor <121710926+zaforsaadik7@users.noreply.github.com>
2023-07-10 22:06:03 +0600

    Create take character as input  form user and stop when input is x.cpp

---
+++ take character as input  form user and stop when input is x.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+   
+  
+     do 
+     {
+       cout<<"Enter a character value and enter x to stop the iteration:";
+        cin>>ch; 
+     }
+     while(ch!= 'x');
+    return 0;
+}
