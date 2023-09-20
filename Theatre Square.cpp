5cf0a5a9115015dd3f3f4721034f819552628641
Emam Zafor <121710926+zaforsaadik7@users.noreply.github.com>
2023-09-20 23:18:52 +0600

    Add files via upload

---
+++ Theatre Square.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long m, n, a, x, y;
+
+    cin>>m>>n>>a;
+
+    if(m%a==0)
+    {
+        x= m/a;
+    }
+    else
+    {
+        x= m/a+1;
+    }
+    if(n%a==0)
+    {
+        y= n/a;
+    }
+    else
+    {
+        y= n/a+1;
+    }
+    cout<<x*y;
+    return 0;
+}
+
